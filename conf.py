b2b407b3b7fe53c80792bfd12d309633b6883f7e
Gunjan Chandani <gunjankaramchandani@gmail.com>
2017-10-17 16:18:20 +0530

    conf.py updated

--- conf.py
+++ conf.py
@@ -36,7 +36,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.mathjax']
+    'sphinx.ext.mathjax',
+    'rinoh.frontend.sphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -126,7 +127,7 @@ htmlhelp_basename = 'OpenDataKitdoc'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+     'papersize': 'letterpaper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
